553df91fe74fe20803a8020eb7b8e7f6830ba4e8
Skochynskiy Dmytro <skodmy@gmail.com>
2018-02-03 15:10:08 +0200

    Currency, Account addedd

--- wallet/models.py
+++ wallet/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Currency(models.Model):
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name_plural = 'currencies'
+
+
+class Account(models.Model):
+    balance = models.FloatField(default=0.0)
+    currency = models.ForeignKey(Currency, models.DO_NOTHING)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
+
+    def __str__(self):
+        return "{}: {:.2f} {}".format(self.user.username, self.balance, self.currency.code)
