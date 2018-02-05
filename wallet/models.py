2aa794b9adedd7ce7834533e070a93aee5a076d7
Skochynskiy Dmytro <skodmy@gmail.com>
2018-02-05 15:20:34 +0200

    active_transaction_id field added to Account

--- wallet/models.py
+++ wallet/models.py
@@ -13,9 +13,10 @@ class Currency(models.Model):
 
 
 class Account(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
     balance = models.FloatField(default=0.0)
     currency = models.ForeignKey(Currency, models.DO_NOTHING)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
+    active_transaction_id = models.BigIntegerField(default=0)
 
     def __str__(self):
         return "{}: {:.2f} {}".format(self.user.username, self.balance, self.currency.code)
